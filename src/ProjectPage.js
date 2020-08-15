bc3269fe913dc2391a91be7ab87afd499461d00a
Russbo <rossbishop@live.co.uk>
2020-08-15 23:50:23 +0100

    Fixed arrayception

--- src/ProjectPage.js
+++ src/ProjectPage.js
@@ -12,7 +12,7 @@ function ProjectPage() {
     const [currentProjectState, updateCurrentProject] = useState({currentDataInt: projectState.projectData.length - 1})             /* Set this to the highest id in the json */
 
     const [galleryState, updateGallery] = useState({galleryData: projectState.projectData[currentProjectState.currentDataInt]});    /* Store the whole current revision in here as gallery is top level */
-    const [commentState, updateComments] = useState([galleryState.galleryData.comments]);                                           /* Store just the comment here */
+    const [commentState, updateComments] = useState(galleryState.galleryData.comments);                                           /* Store just the comment here */
 
     return (
         <>

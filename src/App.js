3ecb71a7dd49fc425f186ca050b2d5e037a5716e
Russbo <rossbishop@live.co.uk>
2020-09-17 21:30:55 +0100

    Start adding authentication with Cognito/Amplify

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import NewProjectPage from './NewProjectPage'
 import TestCss from './TestCss'
 import LoginPage from './LoginPage'
 import RegisterPage from './RegisterPage'
+import Amplify, { Auth } from 'aws-amplify';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+//import awsconfig from './aws-exports';
 
 import {
   BrowserRouter as Router,
@@ -16,6 +19,68 @@ import {
   Link
 } from"react-router-dom";
 import NewRevisionPage from './NewRevisionPage.js';
+
+Amplify.configure(
+  {
+    Auth: {
+
+        // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+        //identityPoolId: 'XX-XXXX-X:XXXXXXXX-XXXX-1234-abcd-1234567890ab',
+
+        // REQUIRED - Amazon Cognito Region
+        region: 'eu-west-2',
+
+        // OPTIONAL - Amazon Cognito Federated Identity Pool Region 
+        // Required only if it's different from Amazon Cognito Region
+        //identityPoolRegion: 'XX-XXXX-X',
+
+        // OPTIONAL - Amazon Cognito User Pool ID
+        userPoolId: 'eu-west-2_YlAzSE1YQ',
+
+        // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+        userPoolWebClientId: '36m2ul34qqlufmf5boonlfvbg3',
+
+        // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
+        //mandatorySignIn: false,
+
+        // OPTIONAL - Configuration for cookie storage
+        // Note: if the secure flag is set to true, then the cookie transmission requires a secure protocol
+        //cookieStorage: {
+        // REQUIRED - Cookie domain (only required if cookieStorage is provided)
+        //    domain: '.yourdomain.com',
+        // OPTIONAL - Cookie path
+        //    path: '/',
+        // OPTIONAL - Cookie expiration in days
+        //    expires: 365,
+        // OPTIONAL - See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie/SameSite
+        //    sameSite: "strict" | "lax",
+        // OPTIONAL - Cookie secure flag
+        // Either true or false, indicating if the cookie transmission requires a secure protocol (https).
+        //    secure: true
+        //},
+
+        // OPTIONAL - customized storage object
+        //storage: MyStorage,
+
+        // OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
+        //authenticationFlowType: 'USER_PASSWORD_AUTH',
+
+        // OPTIONAL - Manually set key value pairs that can be passed to Cognito Lambda Triggers
+        //clientMetadata: { myCustomKey: 'myCustomValue' },
+
+        // OPTIONAL - Hosted UI configuration
+        oauth: {
+            domain: 'art-site.auth.eu-west-2.amazoncognito.com',
+            scope: ['phone', 'email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
+            redirectSignIn: 'http://localhost:3000/',
+            redirectSignOut: 'http://localhost:3000/',
+            responseType: 'code' // or 'token', note that REFRESH token will only be generated when the responseType is code
+          }
+      }
+  }
+);
+
+const currentConfig = Auth.configure();
 
 function App() {
   return (
@@ -62,4 +127,5 @@ function App() {
   )
 }
 
+//export default withAuthenticator(App);
 export default App
--- src/Register.js
+++ src/Register.js
@@ -1,29 +1,58 @@
 import React from 'react'
 
 import 'bootstrap/dist/css/bootstrap.css'
+import { useState } from 'react';
 import RegisterStyles from './css/register.module.css'
 import cx from 'classnames'
 
+import { Auth } from 'aws-amplify';
+
 export default function Register(props) {
+
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
     return (
         <div className="container">
-            <div class="row">
-                <div class="col-12">
+            <div className="row">
+                <div className="col-12">
                     <form className={RegisterStyles.formProfileUpdate}>
                         <h1 className="h3 mb-3 mt-4 font-weight-normal">Register</h1>
                         <label for="inputEmail" className="sr-only">Enter email address</label>
-                        <input type="email" id="inputEmail" className={cx(RegisterStyles.formControl,"mb-3", "py-2", "pl-0")} placeholder="Enter Email Address" required autofocus/>
+                        <input type="email" id="inputEmail" className={cx(RegisterStyles.formControl,"mb-3", "py-2", "pl-0")} placeholder="Enter Email Address" onChange={event => setEmail(event.target.value)} required autofocus/>
                         <label for="inputUsername" className="sr-only">Enter new username</label>
-                        <input type="username" id="inputUsername" className={cx(RegisterStyles.formControl,"mb-3", "py-2", "pl-0")} placeholder="Enter Username" required autofocus/>
+                        <input type="username" id="inputUsername" className={cx(RegisterStyles.formControl,"mb-3", "py-2", "pl-0")} placeholder="Enter Username" onChange={event => setUsername(event.target.value)} required autofocus/>
                         <label for="inputPassword" className="sr-only">Enter password</label>
-                        <input type="password" id="inputPassword" className={cx(RegisterStyles.formControl,"mb-3", "py-2", "pl-0")} placeholder="Enter Password" required autofocus/>
+                        <input type="password" id="inputPassword" className={cx(RegisterStyles.formControl,"mb-3", "py-2", "pl-0")} placeholder="Enter Password" onChange={event => setPassword(event.target.value)} required autofocus/>
                         <label for="inputPasswordConfirm" className="sr-only">Enter password</label>
                         <input type="password" id="inputPasswordConfirm" className={cx(RegisterStyles.formControl,"mb-3", "py-2", "pl-0")} placeholder="Confirm Password" required autofocus/>
-                        <button className={cx(RegisterStyles.btnLogin, "btn", "btn-lg", "btn-primary", "btn-block", "mt-4")} type="submit">Register</button>
+                        <button className={cx(RegisterStyles.btnLogin, "btn", "btn-lg", "btn-primary", "btn-block", "mt-4")} type="submit" onclick={signUp(email,username,password)}>Register</button>
                     </form>
                 </div>
             </div>
         </div>
     )
+}
+
+async function signUp(props) {
+    var username=props.username
+    var password=props.password
+    var email=props.email
+    var birth_date='01/01/1970'
+
+    try {
+        const { user } = await Auth.signUp({
+            username,
+            password,
+            attributes: {
+                email,          // optional
+                birth_date,   // optional - E.164 number convention
+                // other custom attributes 
+            }
+        });
+        console.log(user);
+    } catch (error) {
+        console.log('error signing up:', error);
+    }
 }

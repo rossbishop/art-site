5f1106d46d32227618e5f273fe902754385f162b
Russbo <rossbishop@live.co.uk>
2020-10-25 15:24:59 +0000

    Update both cognito and public username

--- src/ProfileUpdatePage.js
+++ src/ProfileUpdatePage.js
@@ -38,7 +38,7 @@ function ProfileUpdatePage(props) {
     const getUserProfileData = async () => {
         try {
             let user = await Auth.currentAuthenticatedUser()
-            const apiCall = await API.graphql({query: queries.publicUserProfileByUser, variables: {owner: props.userAttribs["preferred_username"]}})
+            const apiCall = await API.graphql({query: queries.publicUserProfileByUser, variables: {owner: props.userDetails.username}})
             console.log(apiCall)
             setLoadProfileSuccess({isSuccess: true, message: "Profile data loaded"})
             setLoadProfileData(apiCall.data.publicUserProfileByUser.items[0])
@@ -65,6 +65,9 @@ function ProfileUpdatePage(props) {
     const updateProfile = async() => {
         try {
             let user = await Auth.currentAuthenticatedUser()
+            let cognitoUpdate = await Auth.updateUserAttributes(user, {
+                'preferred_username': username
+            });
             const publicDetails = {id: user.attributes.sub, username: username, position: job, location: location, bio: bio}
             const publicProfileUpdate = await API.graphql({ query: mutations.updatePublicUserProfile, variables: {input: publicDetails}})
             setProfileSuccess({isSuccess: true, message: "Profile details updated successfully"})

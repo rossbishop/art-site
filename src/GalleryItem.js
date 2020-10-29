c8f02e3c2d7351ec04dc82bf60aa17d95d1233cc
Russbo <rossbishop@live.co.uk>
2020-10-29 10:23:18 +0000

    Project image loading via S3 image component

--- src/GalleryItem.js
+++ src/GalleryItem.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import galleryItemStyles from './css/galleryitem.module.css'
@@ -6,6 +6,25 @@ import './css/carousel.css'
 import { AmplifyS3Image } from '@aws-amplify/ui-react';
 
 export default function GalleryItem(props) {
+
+    useEffect(() => {
+        
+        if((document.querySelector('amplify-s3-image')) != null)
+        {
+            console.log("AMPLIFY IMAGE EXISTS!!!!")
+            let s3Image = document.querySelector('amplify-s3-image')
+            console.log("S3 IMAGE: ")
+            console.log(s3Image)
+            setTimeout(() => {
+                let shadowRootStyles = s3Image.shadowRoot.adoptedStyleSheets
+                shadowRootStyles[0].replace("img {max-width: max-content; margin: auto; width: 100%; height: auto; display: block;}", 1)
+            }, [100])
+        }
+        else
+        {
+            console.log("AMPLIFY IMAGE DOESN'T EXIST!!!")
+        }
+    })
 
     return (
         <div className={((props.selected)==(props.id-1)) ? "carousel-item active" : "carousel-item"}>

d5f6a7153bd31ee2488b758499d8610487a39b92
Russbo <rossbishop@live.co.uk>
2020-11-27 12:31:05 +0000

    Add quotes to vars

--- amplify-pull-push.sh
+++ amplify-pull-push.sh
@@ -17,8 +17,8 @@ AWSCLOUDFORMATIONCONFIG="{\
 \"configLevel\":\"project\",\
 \"useProfile\":false,\
 \"profileName\":\"default\",\
-\"accessKeyId\":$AWS_ACCESS_KEY_ID,\
-\"secretAccessKey\":$AWS_SECRET_ACCESS_KEY,\
+\"accessKeyId\":\"$AWS_ACCESS_KEY_ID\",\
+\"secretAccessKey\":\"$AWS_SECRET_ACCESS_KEY\",\
 \"region\":\"eu-west-2\"\
 }"
 AMPLIFY="{\
